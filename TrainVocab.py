16e01338157755003875b2b35162ea386ef224af
rhugvedd <rhugved.c@gmail.com>
2025-03-07 23:36:16 +0530

    Extremely Sorry! Apologies! - I forgot to upload the generalized version of the GPT project when I uploaded the SansGPT project repo in December 2024. Uploading all the missing files now. These files were developed before December 2024, not recently.

--- TrainVocab.py
+++ TrainVocab.py
@@ -1,24 +1,20 @@
 import torch
 import time
-
 from BPETokenizer import *
 
 """
 Following is a List of Hyperparameters:
 """
-VocabSize = (1024 * 12)
-# File = "Final - Pre-Train Data.txt"
-File = "Components.txt"
-FilePath = "./Data/"
-replacements = {}
-VocabPath = "./Vocab/"
-Load_MergeInfo_Name = ''
-Load_Vocab_Name = ''
-Save_MergeInfo_Name = 'Component_MergeInfo-' + str(VocabSize) + '-'
-Save_Vocab_Name = 'Component_Vocab-' + str(VocabSize) + '-'
+VocabSize = None
+File = ".txt"
+FilePath = "./"
+replacements = {"<br>": "", "<p>": "", "</p>": ""}
+VocabPath = "./"
+Save_MergeInfo_Name = 'Final-Corpus-Tokenizer-Merge-Info-NL-' + str(VocabSize) + '-'
+Save_Vocab_Name = 'Final-Corpus-Tokenizer-Vocab-NL-' + str(VocabSize) + '-'
+WithoutNewLine = False
 SkipFirstChunkInLine = False
-WithoutNewLine = True
-Tokenization_File = './Tokenizer Output/Tokenized-' + str(VocabSize) + '.json'
+Tokenization_File = './Tokenized-Final-Corpus-' + str(VocabSize) + '.json'
 """
 List Ends
 """
@@ -27,7 +23,7 @@ StartTime = time.time()
 
 Tokenizer = BPETokenizer()
 
-Tokenizer.load("./Vocab/", Load_MergeInfo_Name, Load_Vocab_Name)
+# Tokenizer.load("./Vocab/", Load_MergeInfo_Name, Load_Vocab_Name)
 
 TextTokens, Vocab = Tokenizer.TrainVocab(
                                             FilePath + File, 
